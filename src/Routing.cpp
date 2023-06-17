29760aa9db62488b4f2801ed0e5df33e5b0cf783
shenxh-410 <130371034+shenxh-410@users.noreply.github.com>
2023-06-17 20:30:28 +0800

    Update Routing.cpp

--- src/Routing.cpp
+++ src/Routing.cpp
@@ -4,7 +4,9 @@
 void Routing::performFM()
 {
 }
+int cost_of_FM(){
 
+}
 void Routing::connect(std::vector<std::vector<int>>& Maze, int source, int target, std::vector<int> parent)
 {
 	if (source == target) return;
@@ -13,7 +15,6 @@ void Routing::connect(std::vector<std::vector<int>>& Maze, int source, int targe
 	if (Maze[y].at(x) != 2) {
 		Maze[y].at(x) = 3;
 	}
-	Astar.modifyWeight(target, parent[target], 0);
 	connect(Maze, source, parent[target], parent);
 }
 
@@ -26,7 +27,7 @@ void Routing::performAstar()
 	{
 		Astar.Initial();
 		std::vector<int> parent = Astar.AStar(ConnectionPoint[i], ConnectionPoint[i + 1]);
-		connect(Maze, ConnectionPoint[i], ConnectionPoint[i + 1], parent);	
+		connect(Maze, ConnectionPoint[i], ConnectionPoint[i + 1], parent);
 	}
 	Astar.drawGrid(Maze);
 }
@@ -36,7 +37,7 @@ void Routing::outputfile()
 	std::string filepath(filename + "out.txt");
 	std::ofstream output_file(filepath);
 	if (!output_file.is_open()) {
-		std::cout<<"Failed to open output file!" << std::endl;
+		std::cout << "Failed to open output file!" << std::endl;
 		return;
 	}
 	for (auto i : Maze) {
@@ -47,4 +48,13 @@ void Routing::outputfile()
 	// 关闭文件
 	output_file.close();
 	std::cout << "Output file: " << filepath << std::endl;
+}
+int Routing::cost_of_routing_Astar() {
+	int cost = 0;
+	for (std::vector<std::vector<int>>::iterator it = Maze.begin(); it != Maze.end(); it++) {
+		for (std::vector<int>::iterator it2 = it->begin(); it2 != it->end(); it2++)
+			if ((*it2) == 3)
+				cost++;
+	}
+	return cost;
 }
