41796968c84f3aacfda3cbb39891b6e7905d45d7
Maple_of_Ten <1806471942@qq.com>
2023-06-18 11:29:59 +0800

    输出修改

--- src/Routing.cpp
+++ src/Routing.cpp
@@ -4,15 +4,17 @@
 void Routing::performFM()
 {
 }
-int cost_of_FM(){
-
-}
-void Routing::connect(std::vector<std::vector<int>>& Maze, int source, int target, std::vector<int> parent)
+int cost_of_FM()
 {
-	if (source == target) return;
+}
+void Routing::connect(std::vector<std::vector<int>> &Maze, int source, int target, std::vector<int> parent)
+{
+	if (source == target)
+		return;
 	int x = target % Maze[0].size();
 	int y = target / Maze[0].size();
-	if (Maze[y].at(x) != 2) {
+	if (Maze[y].at(x) != 2)
+	{
 		Maze[y].at(x) = 3;
 	}
 	connect(Maze, source, parent[target], parent);
@@ -36,22 +38,38 @@ void Routing::outputfile()
 {
 	std::string filepath(filename + "out.txt");
 	std::ofstream output_file(filepath);
-	if (!output_file.is_open()) {
+	if (!output_file.is_open())
+	{
 		std::cout << "Failed to open output file!" << std::endl;
 		return;
 	}
-	for (auto i : Maze) {
-		for (auto value : i)
-			output_file << value << " ";
-		output_file << std::endl;
+	switch (type)
+	{
+	case OPAstar:
+	// 这里是输出AStar寻径之后的矩阵
+		for (auto i : Maze)
+		{
+			for (auto value : i)
+				output_file << value << " ";
+			output_file << std::endl;
+		}
+		break;
+	case OPFM:
+	// 在这里插入OPFM的输出程序
+		break;
+	default:
+		break;
 	}
+
 	// 关闭文件
 	output_file.close();
 	std::cout << "Output file: " << filepath << std::endl;
 }
-int Routing::cost_of_routing_Astar() {
+int Routing::cost_of_routing_Astar()
+{
 	int cost = 0;
-	for (std::vector<std::vector<int>>::iterator it = Maze.begin(); it != Maze.end(); it++) {
+	for (std::vector<std::vector<int>>::iterator it = Maze.begin(); it != Maze.end(); it++)
+	{
 		for (std::vector<int>::iterator it2 = it->begin(); it2 != it->end(); it2++)
 			if ((*it2) == 3)
 				cost++;
